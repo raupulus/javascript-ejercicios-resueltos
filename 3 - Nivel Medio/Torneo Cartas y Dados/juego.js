3e91fa183112958d24cf85dcc16677210b094a16
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-14 16:18:43 +0100

    Clase juego establecida en hoja externa

--- 3 - Nivel Medio/Torneo Cartas y Dados/juego.js
+++ 3 - Nivel Medio/Torneo Cartas y Dados/juego.js
@@ -3,3 +3,16 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+// Clase padre de JuegoCartas y JuegoDados
+
+/**
+ * Clase Juego que será padre de "JuegoCartas" y  de "JuegoDados" con
+ * los elementos comunes de estos.
+ */
+class Juego {
+    constructor(nombre, jugadores = 2) {
+        this.nombre = nombre;
+        this.jugadores = jugadores;
+    }
+}
