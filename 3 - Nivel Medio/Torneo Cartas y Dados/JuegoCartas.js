159d9d7299b43e6f488a9621d52324069450bb83
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-14 16:26:09 +0100

    Planteado Juego de Cartas como clase externa

--- 3 - Nivel Medio/Torneo Cartas y Dados/JuegoCartas.js
+++ 3 - Nivel Medio/Torneo Cartas y Dados/JuegoCartas.js
@@ -3,3 +3,29 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+/**
+ * Juego de clase Cartas que hereda de la clase "Juego"
+ */
+class JuegoCartas extends Juego {
+    constructor(nombre, jugadores, baraja = 'Española', cartas = 48) {
+        this.baraja = baraja;
+        this.cartas = cartas;
+
+        //this.nombre = nombre;
+        //this.jugadores = jugadores
+
+        // Asignar parámetros heredados
+        //parent.nombre = nombre;
+        //parent.jugadores = jugadores;
+        //parent.constructor(nombre,jugadores);
+    }
+
+    get carateristicas() {
+        caracteristicas = '';
+        // Nombre del padre "Juego"
+        // Jugadores del padre "Juego"
+        caracteristicas += 'Tipo de baraja ' + this.baraja;
+        caracteristicas += 'Número de cartas' + this.cartas;
+    }
+}
