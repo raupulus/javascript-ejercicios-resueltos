353e2fc497a9fe7c7e122d47bde844ff5fddbd9a
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-14 17:01:44 +0100

    Clase de juegos dados externalizada

--- 3 - Nivel Medio/Torneo Cartas y Dados/JuegoDados.js
+++ 3 - Nivel Medio/Torneo Cartas y Dados/JuegoDados.js
@@ -3,3 +3,24 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+// Hereda de Juego
+
+class JuegoDados {
+    constructor(nombre, jugadores, dados, cantidad = 2) {
+        // Constructor del padre
+        super(nombre, jugadores);
+
+        // Propiedades de la propia clase
+        this.dados = dados;
+        this.cantidad = cantidad;
+    }
+
+    get carateristicas() {
+        var caracteristicas = '';
+        caracteristicas += 'Nombre ' + super.nombre;
+        caracteristicas += 'Jugadores ' + super.jugadores;
+        caracteristicas += 'Tipo de dados ' + this.dados;
+        caracteristicas += 'Cantidad de dados' + this.dados;
+    }
+}
