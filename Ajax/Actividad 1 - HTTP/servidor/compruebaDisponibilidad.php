e60b097cf23459e6a3fada77140d50122bc63cee
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-05 19:13:38 +0100

    Base de actividad

---
+++ Ajax/Actividad 1 - HTTP/servidor/compruebaDisponibilidad.php
@@ -0,0 +1,12 @@
+<?php
+    // Generar un número aleatorio
+    srand((double)microtime()*1000000);
+    $numeroAleatorio = rand(0, 10);
+
+    // Simular un falso retardo por la red y el servidor (entre 0 y 3 segundos)
+    sleep($numeroAleatorio % 3);
+
+    // El script devuelve alatoriamente 'si' o 'no' para que la aplicaci�n
+    // cliente pueda comprobar los dos casos
+    echo ($numeroAleatorio % 2 == 0)? "si" : "no";
+?>
