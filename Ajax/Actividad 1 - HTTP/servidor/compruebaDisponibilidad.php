49f38d052dc34dd5921035a0047e285646e029a3
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-06 11:31:17 +0100

    Correcciones mínimas

--- Ajax/Actividad 1 - HTTP/servidor/compruebaDisponibilidad.php
+++ Ajax/Actividad 1 - HTTP/servidor/compruebaDisponibilidad.php
@@ -6,7 +6,7 @@
     // Simular un falso retardo por la red y el servidor (entre 0 y 3 segundos)
     sleep($numeroAleatorio % 3);
 
-    // El script devuelve alatoriamente 'si' o 'no' para que la aplicaci�n
+    // El script devuelve alatoriamente 'si' o 'no' para que la aplicación
     // cliente pueda comprobar los dos casos
     echo ($numeroAleatorio % 2 == 0)? "si" : "no";
 ?>
