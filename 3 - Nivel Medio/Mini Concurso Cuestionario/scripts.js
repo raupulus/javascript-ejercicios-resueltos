f4ee588ea62dc1ce2090988c148cb7817837c1cd
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-17 19:45:07 +0200

    Planteada lógica para resolver ejercicio

--- 3 - Nivel Medio/Mini Concurso Cuestionario/scripts.js
+++ 3 - Nivel Medio/Mini Concurso Cuestionario/scripts.js
@@ -4,9 +4,53 @@
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
-function onInit() {
-    limpiarNodos();
+
+function generarFecha() {
+
 }
 
+function generarNumeros() {
+
+}
+
+function agregarEventos() {
+
+}
+
+function onInit() {
+    limpiarNodos();
+    generarFecha();
+    generarNumeros();
+    agregarEventos();
+}
 
 window.onload = onInit;
+
+function compararFechas() {
+
+    return false;
+}
+
+function compararNumeros() {
+
+    return false;
+}
+
+function mostrarResultado(ganador) {
+    if (ganador) {
+        // Abre ventana felicitando
+    } else {
+        // Abre ventana de perder
+    }
+}
+
+function enviarRespuestas() {
+    var f = compararFechas();
+    var n = compararNumeros();
+
+    if (f === n === true) {
+        mostrarResultado(true);
+    } else {
+        mostrarResultado(false);
+    }
+}
