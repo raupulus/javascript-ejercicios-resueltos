64d840b54f2cbd29148350bdf7be96c4a62170da
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-17 23:00:36 +0200

    Genero datos aleatorios y los comparo con los introducidos por el usuario.

--- 3 - Nivel Medio/Mini Concurso Cuestionario/functions.js
+++ 3 - Nivel Medio/Mini Concurso Cuestionario/functions.js
@@ -3,3 +3,88 @@
  * @copyright Copyright © 2018 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+/**
+ * Genera un número aleatorio según los valores introducidos.
+ * @param minimo Valor mínimo admitido.
+ * @param maximo Valor máximo admitido.
+ * @returns {number} Devuelve el número generado.
+ */
+function generarAleatorio(minimo = 0, maximo = 10) {
+    var x = 0;
+
+    while (true) {
+        x = parseInt((Math.random()+0.01) * maximo);
+
+        if ((x >= minimo) && (x <= maximo)) {
+            return x;
+        }
+    }
+}
+
+/**
+ * Genera una fecha aleatoria entre el 1970 y el 2018.
+ * @returns {string}
+ */
+function generarFechaAleatoria() {
+    var year = generarAleatorio(70, 118);
+    if (year === 100) {
+        year = 2000;
+    } else if (year > 100) {
+        year = 1900 + year;
+    } else {
+        year = Number("19" + String(year));
+    }
+
+    var dia = generarAleatorio(0, 30);
+    var mes = generarAleatorio(0, 11);
+
+    return new Date(year, mes, dia, 0, 0);
+}
+
+/**
+ * Resta las dos fechas recibidas y devuelve la diferencia en milisegundos.
+ * @param inicio
+ * @param fin
+ * @returns {number}
+ */
+function restarFechas(inicio, fin) {
+    return fin.getTime() - inicio.getTime();
+}
+
+
+/**
+ * Recibe milisegundos y devuelve los días que representa.
+ * @param milis
+ * @returns {number}
+ */
+function milisegundosDias(milis) {
+    return Math.floor(milis/(24*60*60*1000));
+}
+
+/**
+ * Recibe milisegundos y devuelve la cantidad de horas que representa
+ * @param milis
+ * @returns {number}
+ */
+function milisegundosHoras(milis) {
+    return Math.floor(milis/(60*60*1000));
+}
+
+/**
+ * Recibe milisegundos y devuelve los minutos que representa.
+ * @param milis
+ * @returns {number}
+ */
+function milisegundosMinutos(milis) {
+    return Math.floor(milis/(60*1000));
+}
+
+/**
+ * Recibe milisegundos y devuelve los segundos que representa.
+ * @param milis
+ * @returns {number}
+ */
+function milisegundosSegundos(milis) {
+    return Math.floor(milis/(60*1000));
+}
--- 3 - Nivel Medio/Mini Concurso Cuestionario/scripts.js
+++ 3 - Nivel Medio/Mini Concurso Cuestionario/scripts.js
@@ -4,47 +4,93 @@
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
+const fechaStart = new Date(0);
+const fechaStop = generarFechaAleatoria();
+const numeros = generarNumeros();
 
-function generarFecha() {
-
-}
-
+/**
+ * Genera 3 números entre el 1 y el 100 de forma aleatoria y los devuelve
+ * @returns {[]} Devuelve un array con 3 números aleatorios del 1 al 100.
+ */
 function generarNumeros() {
-
+    return [generarAleatorio(1, 100),
+            generarAleatorio(1, 100),
+            generarAleatorio(1, 100)
+    ];
 }
 
+/**
+ * Agrega eventos al cuestionario.
+ */
 function agregarEventos() {
-
+    var btn = document.getElementById('btn-test');
+    btn.addEventListener('click', enviarRespuestas);
 }
 
+/**
+ * Pinta los datos generados al cuestionario.
+ */
+function pintarDatos() {
+    var preguntas1 = document.getElementsByClassName('pregunta1');
+    numeros.forEach((ele, idx) => {
+        preguntas1[idx].value = ele;
+    });
+
+    var preguntas2 = document.getElementsByClassName('pregunta2');
+    var milis = restarFechas(fechaStart, fechaStop);
+    preguntas2[0].value = (fechaStop.getDay()+1) + '/' +
+                          (fechaStop.getMonth()+1) + '/' +
+                          fechaStop.getFullYear();
+    console.log('Cantidad de Horas → ' + milisegundosHoras(milis));
+}
+
+/**
+ * Llama a las funciones que necesitan ser llamadas después de cargar la página.
+ */
 function onInit() {
     limpiarNodos();
-    generarFecha();
-    generarNumeros();
     agregarEventos();
+    pintarDatos();
+
+    // Iniciar contador de tiempo
 }
 
 window.onload = onInit;
 
+/**
+ * Compara las horas introducidas por el usuario con las de diferencias.
+ * @returns {boolean} Indica si son iguales (true).
+ */
 function compararFechas() {
-
-    return false;
+    var milis = restarFechas(fechaStart, fechaStop);
+    var horas = milisegundosHoras(milis);
+    var inputUser = document.getElementsByClassName('pregunta2');
+    return horas === inputUser[inputUser.length - 1].value;
 }
 
+/**
+ * Compara el mayor número generado con el introducido por el usuario.
+ * @returns {boolean} Indica si son iguales los números (true).
+ */
 function compararNumeros() {
-
-    return false;
+    var max = Math.max(...numeros);
+    var inputUser = document.getElementsByClassName('pregunta1');
+    return max === inputUser[inputUser.length - 1].value;
 }
 
 function mostrarResultado(ganador) {
-    if (ganador) {
+    if (ganador === true) {
         // Abre ventana felicitando
+        alert('si');
     } else {
         // Abre ventana de perder
+        alert('no');
     }
 }
 
 function enviarRespuestas() {
+    // Parar contador de tiempo
+
     var f = compararFechas();
     var n = compararNumeros();
 
